9aa91461c3d1855a3b5b7502e94577404ea206fe
Jorrit Salverda <jsalverda@travix.com>
2020-06-09 16:32:09 +0200

    retrieve jwt token from api and use it to send the cron event

--- go.mod
+++ go.mod
@@ -4,14 +4,12 @@ go 1.12
 
 require (
 	github.com/alecthomas/kingpin v2.2.6+incompatible
-	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd // indirect
-	github.com/estafette/estafette-foundation v0.0.32
+	github.com/estafette/estafette-ci-contracts v0.0.206
+	github.com/estafette/estafette-foundation v0.0.54
 	github.com/opentracing-contrib/go-stdlib v0.0.0-20190519235532-cf7a6c988dc9
 	github.com/opentracing/opentracing-go v1.1.0
-	github.com/rs/zerolog v1.15.0
+	github.com/rs/zerolog v1.17.2
 	github.com/sethgrid/pester v0.0.0-20190127155807-68a33a018ad0
-	github.com/uber-go/atomic v1.4.0 // indirect
-	github.com/uber/jaeger-client-go v2.16.0+incompatible
-	github.com/uber/jaeger-lib v2.0.0+incompatible // indirect
-	go.uber.org/atomic v1.4.0 // indirect
+	github.com/stretchr/testify v1.3.0
+	github.com/uber/jaeger-client-go v2.20.1+incompatible
 )
--- main.go
+++ main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"io"
-	"net/http"
 	"runtime"
-	"time"
 
 	"github.com/alecthomas/kingpin"
 	foundation "github.com/estafette/estafette-foundation"
-	"github.com/opentracing-contrib/go-stdlib/nethttp"
-	"github.com/opentracing/opentracing-go"
 	"github.com/rs/zerolog/log"
-	"github.com/sethgrid/pester"
 	"github.com/uber/jaeger-client-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 )
@@ -26,7 +20,9 @@ var (
 	buildDate string
 	goVersion = runtime.Version()
 
-	apiKey                = kingpin.Flag("api-key", "The Estafette server passes in this json structure to parameterize the build, set trusted images and inject credentials.").Envar("API_KEY").String()
+	getTokenURL           = kingpin.Flag("get-token-url", "The endpoint on the estafette-ci-api to retrieve a client token from").Envar("GET_TOKEN_URL").String()
+	clientID              = kingpin.Flag("client-id", "The id of the client as configured in Estafette, to securely communicate with the api.").Envar("CLIENT_ID").String()
+	clientSecret          = kingpin.Flag("client-secret", "The secret of the client as configured in Estafette, to securely communicate with the api.").Envar("CLIENT_SECRET").String()
 	ciServerCronEventsURL = kingpin.Flag("cron-events-url", "The endpoint on the estafette-ci-api to post the event to").Envar("CRON_EVENTS_URL").String()
 )
 
@@ -36,48 +32,19 @@ func main() {
 	kingpin.Parse()
 
 	// init log format from envvar ESTAFETTE_LOG_FORMAT
-	foundation.InitLoggingFromEnv(appgroup, app, version, branch, revision, buildDate)
+	foundation.InitLoggingFromEnv(foundation.NewApplicationInfo(appgroup, app, version, branch, revision, buildDate))
 
 	closer := initJaeger(app)
 	defer closer.Close()
 
-	span := opentracing.StartSpan("SendTick")
-	defer span.Finish()
-
-	span.SetBaggageItem("tick-time", time.Now().UTC().Format(time.RFC3339))
-
-	// create client, in order to add headers
-	client := pester.NewExtendedClient(&http.Client{Transport: &nethttp.Transport{}})
-	client.MaxRetries = 3
-	client.Backoff = pester.ExponentialJitterBackoff
-	client.KeepLog = true
-	client.Timeout = time.Second * 10
-
-	request, err := http.NewRequest("POST", *ciServerCronEventsURL, nil)
+	token, err := getToken(*getTokenURL, *clientID, *clientSecret)
 	if err != nil {
-		log.Fatal().Err(err).Msgf("Failed creating http client")
+		log.Fatal().Err(err).Msgf("Failed retrieving JWT token")
 	}
 
-	// add tracing context
-	request = request.WithContext(opentracing.ContextWithSpan(request.Context(), span))
-
-	// collect additional information on setting up connections
-	request, ht := nethttp.TraceRequest(span.Tracer(), request)
-
-	// add headers
-	request.Header.Add("Authorization", fmt.Sprintf("Bearer %v", *apiKey))
-	request.Header.Add("Content-Type", "application/json")
-
-	// perform actual request
-	response, err := client.Do(request)
+	err = sendTick(*ciServerCronEventsURL, token)
 	if err != nil {
-		log.Fatal().Err(err).Str("logs", client.LogString()).Msgf("Failed sending event to %v", *ciServerCronEventsURL)
-	}
-	defer response.Body.Close()
-	ht.Finish()
-
-	if response.StatusCode != http.StatusOK {
-		log.Fatal().Err(err).Str("logs", client.LogString()).Msgf("Failed sending event to %v, response status code %v", *ciServerCronEventsURL, response.StatusCode)
+		log.Fatal().Err(err).Msgf("Failed sending tick")
 	}
 
 	log.Info().Msgf("Sent tick succesfully to %v...", *ciServerCronEventsURL)
@@ -93,7 +60,6 @@ func initJaeger(service string) io.Closer {
 	}
 
 	closer, err := cfg.InitGlobalTracer(service, jaegercfg.Logger(jaeger.StdLogger))
-
 	if err != nil {
 		log.Fatal().Err(err).Msg("Generating Jaeger tracer failed")
 	}
